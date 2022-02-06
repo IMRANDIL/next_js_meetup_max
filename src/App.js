efcdf1a28343502a9140861ba06948192b0fe23e
IMRANDIL <aliimranadil2@gmail.com>
2022-02-06 18:52:04 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favourites from "./pages/Favourites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <MainNavigation />
+
+    <BrowserRouter>
+      <Layout>
         <Routes>
           <Route path="/" element={<AllMeetups />} />
           <Route path="/new-meetup" element={<NewMeetup />} />
@@ -16,8 +17,9 @@ function App() {
 
 
         </Routes>
-      </BrowserRouter>
-    </div>
+      </Layout>
+    </BrowserRouter>
+
   );
 }
 
