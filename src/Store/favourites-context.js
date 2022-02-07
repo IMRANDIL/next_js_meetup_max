750ed89c086a4b6391a622ceb9c7bcc2ad6fbebf
IMRANDIL <aliimranadil2@gmail.com>
2022-02-07 18:05:17 +0530

    yh

--- src/Store/favourites-context.js
+++ src/Store/favourites-context.js
@@ -8,7 +8,28 @@ const FavoritesContext = createContext({
 
 
 function FavoritesContextProvider(props) {
-    const [userFav, setUserFav] = useState([])
+    const [userFav, setUserFav] = useState([]);
+
+
+
+    function addFavHandler(favoriteMeetup) {
+        setUserFav((prevUserFav) => {
+            return prevUserFav.concat(favoriteMeetup)
+        }) //concat returns a new array...
+    }
+
+
+    function removeFavHandler(meetupId) {
+        setUserFav((prevUserFav) => {
+            return prevUserFav.filter((meetup) => meetup.id !== meetupId)
+        })
+    }
+
+
+    function itemIsFavHandler(meetupId) {
+        return userFav.some((meetup) => meetup.id === meetupId)
+    }
+
 
     const context = {
         favourites: userFav,
